47cccef49fab6d9e219e757131116456f21d2c78
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-18 20:33:44 +0600

    register added

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,9 +2,50 @@ import React from 'react';
 
 const Register = () => {
     return (
-        <div>
-            <h2>Please Register</h2>
+        <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col md:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Please Login!</h1>
+           
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+               <input className="btn btn-primary" type="submit" value="Login" />
+              </div>
+            </form>
+          </div>
         </div>
+      </div>
     );
 };
 
