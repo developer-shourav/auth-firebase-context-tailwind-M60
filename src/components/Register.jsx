e2142c394886a404987a98cf2ac3c365d92749e4
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-18 19:53:58 +0600

    register and routes added

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Register = () => {
     return (
         <div>
-            <h2>This is register Page</h2>
+            <h2>Please Register</h2>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,31 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Main from './layout/Main';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main> </Main>,
+    children:[
+      {
+        path:"/",
+        element:<Home> </Home>,
+      },
+
+      {
+       path:"/login",
+       element:<Login> </Login>,
+      },
+
+      {
+        path:"/register",
+        element:<Register> </Register>,
+      },
+      
+    ]
   },
 ]);
 
