23e4aac55ef0a05d78eff90046bb76bb680fe962
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-18 20:01:24 +0600

    header added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <nav>
-            <h2>This is header</h2>
-        </nav>
-    );
+  return (
+    <nav>
+      <div className="navbar bg-primary text-primary-content">
+        <a className="btn btn-ghost normal-case text-xl">AuthMaster</a>
+        <Link to='/' className="btn btn-ghost normal-case text-md">Home</Link>
+        <Link to='/login' className="btn btn-ghost normal-case text-md">Login</Link>
+        <Link to='/register' className="btn btn-ghost normal-case text-md">Register</Link>
+        
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
